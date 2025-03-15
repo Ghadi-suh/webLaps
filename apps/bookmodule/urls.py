8b5793ae8fd67ca5caa1e3b9f3a36e7bf8cf680a
Ghadi-suh <421202093@qu.edu.sa>
2025-03-15 23:38:49 +0300

    lap 6

--- apps/bookmodule/urls.py
+++ apps/bookmodule/urls.py
@@ -13,4 +13,5 @@ urlpatterns = [
     path("html5/text/formatting", views.text_formatting, name="books.text_formatting"),
     path("html5/listing", views.list_elements, name="books.list_elements"),
     path("html5/table", views.table, name="books.table"),
+    path("search/", views.search, name="books.search"),
 ]
